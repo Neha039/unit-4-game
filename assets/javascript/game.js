8c2716bd44a9fbf7977d7cb5cd741593e62bbc9d
Neha Nehru <neha_nehru@hotmail.com>
2019-08-17 00:56:30 -0700

    Made an object of characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+
+var characterList = [
+    OWKenobi = {
+        hPoint:120,
+        aPower: 6,
+        counterAP: 10,
+    },
+    luke = {
+        hPoint:100,
+        aPower:4,
+        counterAP:5,
+    },
+    darthS = {
+        hPoint:150,
+        aPower:12,
+        counterAP:20,
+    },
+    darthM = {
+        hPoint:180,
+        aPower:20,
+        counterAP: 25,
+    },
+];
+
+console.log(characterList);
